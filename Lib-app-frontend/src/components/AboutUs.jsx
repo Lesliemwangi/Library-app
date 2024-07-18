472ff86320e8b1171fc45bd163eb19ca14f94972
Mumo Willis <willykaffiadibaddest06@gmail.com>
2024-07-18 19:02:29 +0300

    committed

---
+++ Lib-app-frontend/src/components/AboutUs.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import "./AboutUs.css"
+
+function AboutUs() {
+  return (
+    <div>
+       <div class="img">
+        <img src="src/assets/images/57a8d20be4f6911a008b479e.webp" alt=""/>
+        <h1><span>#</span>About<span>Us</span></h1>
+    </div>
+    
+    <div class="intro">
+        <card>
+            <div class="container2">
+                <h2>Discover a world of literary wonders with our platform, where every review sparks curiosity, every book finds its reader, and every reader finds their tribe.</h2>
+            </div>
+        </card>
+    </div>
+    <div class="img1">
+        <img src="src/assets/images/636065106196598445-library-ThinkstockPhotos-464729163.webp" alt=""/>
+    </div>
+    <card class="card1">
+        <div class="container">
+            <h1>Our Mission</h1>
+            <p>Empowering readers to discover, share, and connect through authentic book reviews and a vibrant literary
+                community.</p>
+        </div>
+    </card>
+    <div class="img2">
+        <img src="src/assets/images/desktop-wallpaper-old-books-cool-library.jpg" alt=""/>
+    </div>
+    <card class="card1">
+        <div class="container1">
+            <h1>Our Vision</h1>
+            <p>To be the premier platform for book lovers, fostering a culture of informed and passionate reading.</p>
+        </div>
+        <div class="img3">
+            <img src="src/assets/images/seoul-ig-5.jpg" alt=""/>
+        </div>
+    </card>
+    <card class="card1">
+        <div class="container">
+            <h1>What We Bring to the Table</h1>
+            <p>A treasure trove of honest reviews, lively book discussions, and a community of bookworms who never judge a book by its cover (or your guilty pleasure reads)</p>
+
+        </div>
+    </card>
+    </div>
+  )
+}
+
+export default AboutUs

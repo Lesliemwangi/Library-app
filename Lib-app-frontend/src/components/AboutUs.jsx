4d0f8e1ffedbf52f38a212075c932d5ceb884133
Mumo Willis <willykaffiadibaddest06@gmail.com>
2024-07-23 21:49:45 +0300

    done

--- Lib-app-frontend/src/components/AboutUs.jsx
+++ Lib-app-frontend/src/components/AboutUs.jsx
@@ -1,51 +1,37 @@
 import React from 'react'
 import "./AboutUs.css"
 
+
 function AboutUs() {
   return (
-    <div>
-       <div class="img">
-        <img src="src/assets/images/57a8d20be4f6911a008b479e.webp" alt=""/>
-        <h1><span>#</span>About<span>Us</span></h1>
-    </div>
-    
-    <div class="intro">
-        <card>
-            <div class="container22">
-                <h2>Discover a world of literary wonders with our platform, where every review sparks curiosity, every book finds its reader, and every reader finds their tribe.</h2>
-            </div>
-        </card>
-    </div>
-    <div class="img1">
-        <img src="src/assets/images/636065106196598445-library-ThinkstockPhotos-464729163.webp" alt=""/>
-    </div>
-    <card class="card1">
-        <div class="container11">
-            <h1>Our Mission</h1>
+    <>
+   <div className="intro">
+    <h1>ABOUT US</h1>
+    <img src="https://media.istockphoto.com/id/1498878143/photo/book-stack-and-open-book-on-the-desk-in-modern-public-library.webp?b=1&s=170667a&w=0&k=20&c=T63zJBKuZLTUQwwAAwLzbMwtzAEdd4wZRaEV6EAdUnA=" alt="" />
+   </div>
+   <div className="intro2">
+    <h3>"Lets read,who knows;You might learn something" </h3>
+   </div>
+   <div className="intro3">
+    <div className="card1">
+        <div className="container11">
+            <h3>Our Mission</h3>
             <p>Empowering readers to discover, share, and connect through authentic book reviews and a vibrant literary
-                community.</p>
+            community.</p>
         </div>
-    </card>
-    <div class="img2">
-        <img src="src/assets/images/desktop-wallpaper-old-books-cool-library.jpg" alt=""/>
-    </div>
-    <card class="card1">
-        <div class="container12">
+        <div className="random1">
+            <h1>"Create distinction...</h1>
+        </div>
+        <div className="random2">
+            <h1>...become better"</h1>
+        </div>
+        <div class="container1">
             <h1>Our Vision</h1>
             <p>To be the premier platform for book lovers, fostering a culture of informed and passionate reading.</p>
         </div>
-        <div class="img3">
-            <img src="src/assets/images/seoul-ig-5.jpg" alt=""/>
-        </div>
-    </card>
-    <card class="card1">
-        <div class="container">
-            <h1>What We Bring to the Table</h1>
-            <p>A treasure trove of honest reviews, lively book discussions, and a community of bookworms who never judge a book by its cover (or your guilty pleasure reads)</p>
-
     </div>
-</card>
-</div>
+   </div>
+   </>
   )
 }
 

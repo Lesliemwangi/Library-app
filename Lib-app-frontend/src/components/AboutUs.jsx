c364ae0c3282613cfec84675d3a3a581711b965c
Lesliemwangi <luciannahphoi@gmail.com>
2024-07-24 09:48:09 +0300

    final

--- Lib-app-frontend/src/components/AboutUs.jsx
+++ Lib-app-frontend/src/components/AboutUs.jsx
@@ -6,7 +6,7 @@ function AboutUs() {
   return (
     <>
    <div className="intro">
-    <h1>ABOUT US</h1>
+    <h1 style={{ fontSize: '3.0rem', textDecoration: 'underline'}}>ABOUT US</h1>
     <img src="https://media.istockphoto.com/id/1498878143/photo/book-stack-and-open-book-on-the-desk-in-modern-public-library.webp?b=1&s=170667a&w=0&k=20&c=T63zJBKuZLTUQwwAAwLzbMwtzAEdd4wZRaEV6EAdUnA=" alt="" />
    </div>
    <div className="intro2">
